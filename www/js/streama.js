6096ab0818f825afd683ee3f65d9ff50acbf5d00
dularion <antonia.hulha@systaro.de>
2017-02-09 00:37:33 +0100

    add ionic services API key

--- www/js/streama.js
+++ www/js/streama.js
@@ -4,20 +4,28 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in setup.controller.js
-angular.module('streama', ['ionic', 'streama.setup', 'streama.core'])
+angular.module('streama', ['ionic', 'ionic.cloud', 'streama.setup', 'streama.core'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+  .run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-});
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
+    });
+  })
+
+	.config(function($ionicCloudProvider) {
+		$ionicCloudProvider.init({
+			"core": {
+				"app_id": "485e5f3ff58323ecca1684e58b4e9eaabcc6d5e19e74b299"
+			}
+		});
+	});
