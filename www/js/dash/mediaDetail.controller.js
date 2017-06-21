170e36c9290c38e8617ddd9d5491393877a29512
dularion <antonia.hulha@systaro.de>
2017-06-21 22:57:36 +0200

    add continueWatching method to mediaDetail - WIP

--- www/js/dash/mediaDetail.controller.js
+++ www/js/dash/mediaDetail.controller.js
@@ -1,8 +1,23 @@
 angular.module('streama.dash')
-.controller('MediaDetailCtrl', function($stateParams, apiService) {
+.controller('MediaDetailCtrl', function($stateParams, apiService, $state) {
 	var vm = this;
-
 	console.log('%c $stateParams', 'color: deeppink; font-weight: bold; text-shadow: 0 0 5px deeppink;', $stateParams);
+
+	vm.continueWatching = continueWatching;
+	
+	
+	function continueWatching() {
+		if($stateParams.mediaType === 'tvShow'){
+			apiService.dash.cotinueWatching({id: $stateParams.mediaId}).then(function (response) {
+				var videoInstance = response.data;
+				console.log('%c videoInstance', 'color: deeppink; font-weight: bold; text-shadow: 0 0 5px deeppink;', videoInstance);
+				$state.go('player', {videoId: videoInstance.id});
+			});
+		}else{
+			$state.go('player', {videoId: $stateParams.mediaId});
+		}
+	}
+
 
 	apiService.dash.mediaDetail({id: $stateParams.mediaId, mediaType: $stateParams.mediaType}).then(function (response) {
 		vm.media = response.data;
