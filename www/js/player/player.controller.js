f1c46cef08764c60fecb33eba8b452dd8c9a7b45
dularion <antonia.hulha@systaro.de>
2017-06-21 23:01:11 +0200

    add episode-fetching via data.show.id

--- www/js/player/player.controller.js
+++ www/js/player/player.controller.js
@@ -12,7 +12,11 @@ angular.module('streama.player').controller('PlayerCtrl', [
 			var missingFileError = playerService.handleMissingFileError(vm.video);
 			var wrongBasePathError = playerService.handleWrongBasepathError(vm.video);
 
-			if(!missingFileError && !wrongBasePathError){
+			if(data.mediaType === 'episode' && data.show){
+				apiService.dash.listEpisodesForShow({id: data.show.id}).success(function (episodes) {
+					vm.videoOptions = playerService.setVideoOptions(vm.video, episodes);
+				});
+			}else{
 				vm.videoOptions = playerService.setVideoOptions(vm.video);
 			}
 
