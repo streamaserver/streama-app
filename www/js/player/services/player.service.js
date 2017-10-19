f06c34ef644d9b19f3b816daf88b44bd7c1bf1ee
dularion <antonia.hulha@systaro.de>
2017-10-19 23:00:12 +0200

    add videoStillImage for player-settings

--- www/js/player/services/player.service.js
+++ www/js/player/services/player.service.js
@@ -36,6 +36,7 @@ angular.module('streama.player').factory('playerService',
 
       videoOptions.videoSrc = _.get(video, 'files[0].src');
       videoOptions.isExternalLink = true;
+      videoOptions.videoStillImage = _.get(video, 'still_path');
       videoOptions.videoMetaTitle = _.get(video, 'title') || _.get(video, 'episodeString') + ' ' + _.get(video, 'name');
       videoOptions.episodeList = _.groupBy(episodes, 'season_number');
 
