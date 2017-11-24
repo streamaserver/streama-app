9a3252d074cb32ff88d5d21a968721de03c3ac8b
dularion <antonia.hulha@systaro.de>
2017-11-25 00:02:58 +0100

    jshint happy

--- www/js/dash/dash.controller.js
+++ www/js/dash/dash.controller.js
@@ -1,58 +1,59 @@
+(function () {
 'use strict';
 
-angular.module('streama.dash')
-.controller('DashCtrl', function(currentUser, apiService, $ionicSideMenuDelegate, $ionicScrollDelegate, $state, $scope) {
-	var vm = this;
-	vm.openMediaDetail = openMediaDetail;
-  $scope.$on('$stateChangeSuccess', onStateChangeSuccess);
-	init();
+  angular.module('streama.dash')
+  .controller('DashCtrl', function(currentUser, apiService, $ionicSideMenuDelegate, $ionicScrollDelegate, $state, $scope) {
+    var vm = this;
+    vm.openMediaDetail = openMediaDetail;
+    $scope.$on('$stateChangeSuccess', onStateChangeSuccess);
+    init();
 
 
-	function init() {
-		$ionicSideMenuDelegate.canDragContent(false);
-		$ionicScrollDelegate.freezeAllScrolls(true);
-		vm.swiperOptions = initSwiperOptions();
+    function init() {
+      $ionicSideMenuDelegate.canDragContent(false);
+      $ionicScrollDelegate.freezeAllScrolls(true);
+      vm.swiperOptions = initSwiperOptions();
 
-		apiService.dash.listContinueWatching().then(function (response) {
-			vm.continueWatchingList = response.data;
-		});
-		apiService.dash.listShows().then(function (response) {
-			vm.shows = response.data;
-		});
-		apiService.dash.listMovies().then(function (response) {
-			vm.movies = response.data;
-		});
-		apiService.dash.listGenericVideos().then(function (response) {
-			vm.genericVideos = response.data;
-		});
-		apiService.dash.listNewReleases().then(function (response) {
-			vm.newReleases = response.data;
-		});
-		apiService.dash.listRecommendations().then(function (response) {
-			vm.recommendations = response.data;
-		});
-	}
-
-	function onStateChangeSuccess(e, toState) {
-	  if(toState.name === 'main.dash'){
-	    init();
+      apiService.dash.listContinueWatching().then(function (response) {
+        vm.continueWatchingList = response.data;
+      });
+      apiService.dash.listShows().then(function (response) {
+        vm.shows = response.data;
+      });
+      apiService.dash.listMovies().then(function (response) {
+        vm.movies = response.data;
+      });
+      apiService.dash.listGenericVideos().then(function (response) {
+        vm.genericVideos = response.data;
+      });
+      apiService.dash.listNewReleases().then(function (response) {
+        vm.newReleases = response.data;
+      });
+      apiService.dash.listRecommendations().then(function (response) {
+        vm.recommendations = response.data;
+      });
     }
-  }
 
-	function openMediaDetail(media) {
-		var options = { reload: false };
-		$state.go('main.mediaDetail', {mediaId: media.id}, options);
-	}
+    function onStateChangeSuccess(e, toState) {
+      if(toState.name === 'main.dash'){
+        init();
+      }
+    }
 
-	function initSwiperOptions() {
-		return {
-			pagination: '.swiper-pagination',
-			slidesPerView: 'auto',
-			paginationClickable: true,
-			spaceBetween: 8,
-			freeMode: true
-		};
-	}
+    function openMediaDetail(media) {
+      var options = { reload: false };
+      $state.go('main.mediaDetail', {mediaId: media.id}, options);
+    }
 
-});
+    function initSwiperOptions() {
+      return {
+        pagination: '.swiper-pagination',
+        slidesPerView: 'auto',
+        paginationClickable: true,
+        spaceBetween: 8,
+        freeMode: true
+      };
+    }
 
+  });
+}());
