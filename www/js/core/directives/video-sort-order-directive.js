7b8771b53d1f3f7aac6ea80235f8f0f1dc1120e1
dularion <antonia.hulha@systaro.de>
2017-12-13 23:19:28 +0100

    add video sorting to dashboard for v1.4.1 and higher

---
+++ www/js/core/directives/video-sort-order-directive.js
@@ -0,0 +1,49 @@
+angular.module('streama.core').directive('videoSortOrderDropdown', [function () {
+		return {
+		  require: 'ngModel',
+			restrict: 'AE',
+			templateUrl: 'templates/core/video-sort-order-dropdown.html',
+			scope: {
+				dropdownType: '='
+			},
+			link: function ($scope, $elem, $attrs, $controller) {
+
+        $controller.$formatters.push(formatter);
+
+				$scope.sortOrders = [];
+
+				if($scope.dropdownType == 'movie'){
+					$scope.sortOrders = $scope.sortOrders.concat([
+						{sort: 'title', order: 'ASC', label: 'SORT_OPTIONS.AZ'},
+						{sort: 'title', order: 'DESC', label: 'SORT_OPTIONS.ZA'},
+						{sort: 'release_date', order: 'DESC', label: 'SORT_OPTIONS.NEWEST_RELEASED'},
+						{sort: 'release_date', order: 'ASC', label: 'SORT_OPTIONS.OLDEST_RELEASED'}
+					])
+				}
+
+				if($scope.dropdownType == 'tvShow'){
+					$scope.sortOrders = $scope.sortOrders.concat([
+						{sort: 'name', order: 'ASC', label: 'SORT_OPTIONS.AZ'},
+						{sort: 'name', order: 'DESC', label: 'SORT_OPTIONS.ZA'},
+						{sort: 'first_air_date', order: 'DESC', label: 'SORT_OPTIONS.NEWEST_AIRED'},
+						{sort: 'first_air_date', order: 'ASC', label: 'SORT_OPTIONS.OLDEST_AIRED'}
+					])
+				}
+
+				$scope.sortOrders = $scope.sortOrders.concat([
+					{sort: 'dateCreated', order: 'DESC', label: 'SORT_OPTIONS.NEWEST_ADDED'},
+					{sort: 'dateCreated', order: 'ASC', label: 'SORT_OPTIONS.OLDEST_ADDED'}
+				]);
+
+        function formatter(value) {
+          $scope.currentSort = _.find($scope.sortOrders, value) || $scope.sortOrders[0];
+        }
+
+				$scope.setCurrentSort = function (sortOrder) {
+					$scope.currentSort = sortOrder;
+          $controller.$setViewValue(sortOrder);
+				};
+
+			}
+		}
+	}]);
