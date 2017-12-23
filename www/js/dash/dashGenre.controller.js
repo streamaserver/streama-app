bade46ee431d409434677dd072dcc4f5e896340e
dularion <antonia.hulha@systaro.de>
2017-12-23 22:31:01 +0100

    add swiperOptions for dashGenre and enable other media types

--- www/js/dash/dashGenre.controller.js
+++ www/js/dash/dashGenre.controller.js
@@ -4,6 +4,7 @@
   angular.module('streama.dash')
   .controller('DashGenreCtrl', function($stateParams, $rootScope, apiService, mediaListService, $ionicSideMenuDelegate, $scope) {
     var vm = this;
+    vm.swiperOptions = mediaListService.initSwiperOptions();
 
     init();
 
@@ -12,8 +13,8 @@
       $ionicSideMenuDelegate.canDragContent(false);
       $scope.mainVm.selectedGenre = _.find($scope.mainVm.genres, {id: parseInt($stateParams.genreId)});
 
-      // vm.show = mediaListService.init(apiService.dash.listShows, {sort: 'name', order: 'ASC'});
-      // vm.movie = mediaListService.init(apiService.dash.listMovies, {sort: 'title', order: 'ASC'});
+      vm.show = mediaListService.init(apiService.dash.listShows, {sort: 'name', order: 'ASC'});
+      vm.movie = mediaListService.init(apiService.dash.listMovies, {sort: 'title', order: 'ASC'});
       vm.genericVideo = mediaListService.init(apiService.dash.listGenericVideos, {sort: 'title', order: 'ASC'});
     }
 
