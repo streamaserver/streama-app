fc9be8d04f753c26abca6614af51c3c79aa2710f
Maxim Bondarenko <maximbndrnk@gmail.com>
2018-12-10 16:23:47 +0200

    add grey color to highlight current profile

--- www/js/app.interceptor.js
+++ www/js/app.interceptor.js
@@ -8,7 +8,6 @@ angular.module('streama').config(function ($httpProvider) {
 angular.module('streama').factory('httpInterceptor', function ($rootScope, $q, localStorageService) {
   return {
     request: function (config) {
-      config.params = config.params || {};
       if (localStorageService.get('currentProfile')){
         config.headers.profileId = localStorageService.get('currentProfile').id || 0;
       }
