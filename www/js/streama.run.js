1116dc2a0710813de24115bf1fae7e5bc83071d9
dularion <antonia.hulha@systaro.de>
2019-01-16 20:14:47 +0100

    fix jshint errors for streama.run.js

--- www/js/streama.run.js
+++ www/js/streama.run.js
@@ -1,38 +1,42 @@
-angular.module('streama')
-  .run(function($ionicPlatform, $rootScope, apiService, $state, localStorageService) {
+(function(){
+  'use strict';
 
-		$rootScope.logout = logout;
-		$rootScope.changeServerConnection = changeServerConnection;
-		$rootScope.serverBasePath = localStorageService.get('streamaDomain');
+  angular.module('streama')
+    .run(function($ionicPlatform, $rootScope, apiService, $state, localStorageService) {
 
-    $ionicPlatform.ready(onPlatformReady);
-		$rootScope.navLogo = '<img class="title-image" src="img/logo.png" />';
+      $rootScope.logout = logout;
+      $rootScope.changeServerConnection = changeServerConnection;
+      $rootScope.serverBasePath = localStorageService.get('streamaDomain');
+
+      $ionicPlatform.ready(onPlatformReady);
+      $rootScope.navLogo = '<img class="title-image" src="img/logo.png" />';
 
 
-		function onPlatformReady() {
-			// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-			// for form inputs)
-			if (window.cordova && window.cordova.plugins.Keyboard) {
-				cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-				cordova.plugins.Keyboard.disableScroll(true);
+      function onPlatformReady() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+          cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+          cordova.plugins.Keyboard.disableScroll(true);
 
-			}
-			if (window.StatusBar) {
-				StatusBar.hide();
-			}
-		}
+        }
+        if (window.StatusBar) {
+          StatusBar.hide();
+        }
+      }
 
-		function logout() {
-			apiService.core.logout().then(function () {
-				$state.go('setup');
-			});
-		}
+      function logout() {
+        apiService.core.logout().then(function () {
+          $state.go('setup');
+        });
+      }
 
-		function changeServerConnection() {
-			apiService.core.logout().then(function () {
-			  apiService.setBasePath(null);
-        localStorageService.remove('streamaDomain');
-				$state.go('setup');
-			});
-		}
-  });
+      function changeServerConnection() {
+        apiService.core.logout().then(function () {
+          apiService.setBasePath(null);
+          localStorageService.remove('streamaDomain');
+          $state.go('setup');
+        });
+      }
+    });
+})();
